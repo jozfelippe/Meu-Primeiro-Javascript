db07b4c378e506747b3e27ce480cad80b02b47f8
Jose Felipe da Silva Santos <josefelipesilvasantos874@gmail.com>
2022-10-20 22:38:54 -0300

    Finalizado

---
+++ exemplo.js
@@ -0,0 +1,5 @@
+console.log('Olá Mundo js!');
+console.log(2+2);
+
+//comentários da aula: esta aula foi muito produtiva e legal, por icrivel que pareça, pois raramente eu fico na máquina ai eu fico sem ter o que fazer
+/*
